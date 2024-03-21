ac3ee91246a827904086847432f47ebf579e85b0
bhanu247 <163824846+bhanu247@users.noreply.github.com>
2024-03-21 15:58:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your JavaScript code goes here
-    console.log("ESS Portal ready!");
-});
+function validateLogin() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    var dummyEmail = "bhanu.p@hlbhamt.com";
+    var dummyPassword = "Apple@9211";
+
+    if(email === dummyEmail && password === dummyPassword) {
+        window.location.href = 'dashboard.html'; // Redirect to the dashboard page
+        return false; // Prevent default form submission
+    } else {
+        alert('Invalid Credentials'); // User feedback
+        return false; // Prevent default form submission
+    }
+}
